0be02ba91a3fcbdd61ad178a082304a7602d5ec6
rachittt <guptarachit2004@gmail.com>
2019-07-01 19:22:02 +0530

    ui

--- app/src/main/java/com/syncx/spot/SignUp.java
+++ app/src/main/java/com/syncx/spot/SignUp.java
@@ -9,24 +9,13 @@ import android.widget.Button;
 
 public class SignUp extends AppCompatActivity {
 
-    CardView cardView;
-    private Button btnLogin;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
-        btnLogin = findViewById(R.id.btnLogin);
-        btnLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                btnLogin();
-            }
-        });
     }
-        private void btnLogin(){
-            Intent intent = new Intent(SignUp.this,login.class);
-            startActivity(intent);
-        }
+
 }
