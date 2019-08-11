896947c522475748677d2e6df391af905559fc6f
Oorjit Chowdhary <oorjit.chowdhary@hotmail.com>
2019-08-11 19:42:58 +0530

    bottom navigation bar

--- app/src/main/java/com/syncx/spot/nearMe.java
+++ app/src/main/java/com/syncx/spot/nearMe.java
@@ -1,7 +1,12 @@
 package com.syncx.spot;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
 
 public class nearMe extends AppCompatActivity {
 
@@ -9,5 +14,25 @@ public class nearMe extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_near_me);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navbar);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()){
+                    case R.id.action_nearme:
+                        startActivity(new Intent(nearMe.this, nearMe.class));
+                        break;
+                    case R.id.action_weather:
+                        startActivity(new Intent(nearMe.this, weather.class));
+                        break;
+                    case R.id.action_profile:
+                        startActivity(new Intent(nearMe.this, profile.class));
+                        break;
+                }
+                return true;
+            }
+        });
+
     }
 }
--- app/src/main/java/com/syncx/spot/weather.java
+++ app/src/main/java/com/syncx/spot/weather.java
@@ -1,7 +1,11 @@
 package com.syncx.spot;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class weather extends AppCompatActivity {
 
@@ -9,5 +13,24 @@ public class weather extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navbar);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()){
+                    case R.id.action_nearme:
+                        startActivity(new Intent(weather.this, nearMe.class));
+                        break;
+                    case R.id.action_weather:
+                        startActivity(new Intent(weather.this, weather.class));
+                        break;
+                    case R.id.action_profile:
+                        startActivity(new Intent(weather.this, profile.class));
+                        break;
+                }
+                return true;
+            }
+        });
     }
 }
