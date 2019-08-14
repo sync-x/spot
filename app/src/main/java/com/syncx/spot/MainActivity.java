9b5a23db35dd45e35049f04842c00922ff1c002e
Oorjit Chowdhary <oorjit.chowdhary@hotmail.com>
2019-08-15 01:58:29 +0530

    bottom navigation bar

--- app/src/main/java/com/syncx/spot/MainActivity.java
+++ app/src/main/java/com/syncx/spot/MainActivity.java
@@ -2,9 +2,13 @@ package com.syncx.spot;
 
 import android.content.Intent;
 import android.graphics.Color;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.RelativeLayout;
@@ -14,34 +18,42 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
     FirebaseAuth mAuth;
-    RelativeLayout relativeLayout;
-    Snackbar snackbar;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        relativeLayout = findViewById(R.id.layoutSnackbar);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_nav);
+        bottomNavigationView.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
     }
 
-    public void logOutUser(View view){
+    private BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+            Fragment selectedFragment = null;
+            switch (menuItem.getItemId()){
+                case R.id.action_classify:
+                    selectedFragment = new ClassifyFragment();
+                    break;
+                case R.id.action_weather:
+                    selectedFragment = new WeatherFragment();
+                    break;
+                case R.id.action_nearme:
+                    selectedFragment = new NearMeFragment();
+                    break;
+                case R.id.action_profile:
+                    selectedFragment = new ProfileFragment();
+                    break;
+            }
+
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+            return true;
+        }
+    };
+
+    public void logOut(View view){
         mAuth.getInstance().signOut();
-        // add snackbar
-        showSnackbar();
         startActivity(new Intent(MainActivity.this, LoginActivity.class));
-    }
-    public void showSnackbar(){
-        Snackbar snackbar = Snackbar.make(relativeLayout,"Log Out successful",Snackbar.LENGTH_LONG).setDuration(2000);
-        snackbar.show();
-    }
-    public void goToProfile(View view){
-        startActivity(new Intent(MainActivity.this,profile.class));
-    }
-    public void goToNearMe(View view){
-        startActivity(new Intent(MainActivity.this,nearMe.class));
-    }
-    public void goToWeather(View view){
-        startActivity(new Intent(MainActivity.this,weather.class));
     }
 }
